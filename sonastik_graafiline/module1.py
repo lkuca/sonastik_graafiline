309d569618c81294ac4797f00b6575b40b74fa6d
lkuca <120181604+lkuca@users.noreply.github.com>
2023-03-30 12:28:35 +0300

    Update module1.py

--- sonastik_graafiline/module1.py
+++ sonastik_graafiline/module1.py
@@ -1,23 +1,34 @@
 from pickle import LIST
 from tkinter import *
 import random
+global vene_list, eesti_list
 def Loe_failist(fail:str)->list:
-    """
-    """
     f=open(fail,"r",encoding="utf-8-sig")
     jarjend=[]
     for rida in f:
         jarjend.append(rida.strip())
     f.close()
     return jarjend
-aken1=Tk()
-aken1.geometry("1920x1080")
-valik=Entry(fg="blue",text=f"1-translate\n2-lisa sona\n3-muuda sona\n4-mang\n ",bg="lightblue",width=15, font="Arial 20",justify=CENTER)
-vene_letters =["а", "б", "в", "г","д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
-sona=Label(text="sissesta sona:",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
-sonavastus=Entry(fg="blue",bg="lightblue",width=15, font="Arial 20",justify=CENTER)
-resultsõnast=Label(aken,text=f"{sona} on vene keeles",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
-eesti_sõna=Label(aken,text=f"{sona} - {vaste}",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
-vale=Label(aken,text="sona ei ole leitud",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
-resultsõnast1=Label(aken,text=f"{sona} on inglise keeles",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
-vene_sõna=Label(aken,text=f"{sona} - {vaste}",bg="silver",fg="#AA4A44",font="Arial 20",height=5,width=15)
+def tõlkida():
+    vene_letters =["а", "б", "в", "г","д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
+    aken=Tk()
+    aken.geometry("800x500")
+    tran=Label(aken, text="Sisesta sõna mis tahad tõlkida:",bg="silver",fg="#AA4A44",font="Arial 20",height=1,width=55)
+    translate=Entry(aken, text="Sisesta sõna mis tahad tõlkida:",bg="silver",fg="#AA4A44",font="Arial 20",width=55)
+    tran.pack()
+    translate.pack()
+    def tõlkida_check(event):
+        trin=translate.get()
+        if trin.lower()[0] in vene_letters:
+            vast=Label(aken, text="{trin} on vene keeles:",bg="silver",fg="#AA4A44",font="Arial 20",height=1,width=55)
+            vast.pack()
+            try:
+                ind = vene_list.index(trin)
+                vaste = eesti_list[ind]
+                trun=Label(aken, text=f"{trin} - {vaste}",bg="silver",fg="#AA4A44",font="Arial 20",height=1,width=55)
+                trun.pack()
+            except ValueError:
+                tron=Label(aken, text=f"sona ei ole leitud",bg="silver",fg="#AA4A44",font="Arial 20",height=1,width=55)
+                tron.pack()
+    aken.bind("<Return>",tõlkida_check)
+    aken.mainloop()
